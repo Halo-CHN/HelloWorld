a9c7076e6660654228a9d48ddffbb17e7ac7c91a
Halo-CHN <halo-chn@outlook.com>
2015-06-18 18:10:49 +0800

    add imageloader

--- HelloWorld/src/com/chn/halo/core/ApplicationEx.java
+++ HelloWorld/src/com/chn/halo/core/ApplicationEx.java
@@ -1,6 +1,16 @@
 package com.chn.halo.core;
 
+import java.io.File;
 import java.util.List;
+
+import com.hengyirong.GlobalConstants;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
@@ -57,6 +67,46 @@ public class ApplicationEx extends Application {
 		}
 		return false;
 	}
+
+	public static void initImageLoader(Context context) {
+		
+		File cacheDir = StorageUtils.getOwnCacheDirectory(context, "halo/img");//缓存路径
+
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+				context).threadPriority(Thread.NORM_PRIORITY - 2)
+				.denyCacheImageMultipleSizesInMemory()// 当同一个Uri获取不同大小的图片，缓存到内存时，只缓存一个。默认会缓存多个不同的大小的相同图片
+				.diskCacheFileNameGenerator(new Md5FileNameGenerator())// 将保存的时候的URI名称用MD5加密
+				.diskCacheSize(50 * 1024 * 1024) // 50 Mb
+				.tasksProcessingOrder(QueueProcessingType.LIFO) // 设置图片下载和显示的工作队列排序
+				.diskCache(new UnlimitedDiskCache(cacheDir))// 自定义缓存路径
+				/**
+				 * FileCountLimitedDiscCache（可以设定缓存图片的个数，当超过设定值，删除掉最先加入到硬盘的文件）
+				 * LimitedAgeDiscCache（设定文件存活的最长时间，当超过这个值，就删除该文件）
+				 * TotalSizeLimitedDiscCache（设定缓存bitmap的最大值，当超过这个值，删除最先加入到硬盘的文件）
+				 * UnlimitedDiscCache（这个缓存类没有任何的限制）
+				 */
+				// .writeDebugLogs() // Remove for release app
+				.build();
+		// Initialize ImageLoader with configuration.
+		ImageLoader.getInstance().init(config);
+	}
+
+	/**
+	 * ImageLoader是根据ImageView的height，width确定图片的宽高。 如果经常出现OOM，进行以下设置
+	 * ①减少配置之中线程池的大小，(.threadPoolSize).推荐1-5；
+	 * ②使用.bitmapConfig(Bitmap.config.RGB_565)代替ARGB_8888;
+	 * ③使用.imageScaleType(ImageScaleType.IN_SAMPLE_INT) 或者
+	 * try.imageScaleType(ImageScaleType.EXACTLY)；
+	 * ④避免使用RoundedBitmapDisplayer.他会创建新的ARGB_8888格式的Bitmap对象；
+	 * ⑤使用.memoryCache(new WeakMemoryCache())，不要使用.cacheInMemory();
+	 * 
+	 * 如何加载本地图片 String imageUri = "http://site.com/image.png"; // from Web
+	 * String imageUri = "file:///mnt/sdcard/image.png"; // from SD card String
+	 * imageUri = "content://media/external/audio/albumart/13"; // from content
+	 * provider String imageUri = "assets://image.png"; // from assets String
+	 * imageUri = "drawable://" + R.drawable.image; // from drawables (only
+	 * images, non-9patch)
+	 */
 
 	/**
 	 * 取得DaoMaster
